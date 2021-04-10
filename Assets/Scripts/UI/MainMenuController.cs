eacae358cbec1a596b89c81b004abddffb441c61
sinuhePL <lawicki.karol@gmail.com>
2021-04-10 17:58:21 +0200

    Added instructions button and text to main menu

--- Assets/Scripts/UI/MainMenuController.cs
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -8,12 +8,14 @@ using DG.Tweening;
 public class MainMenuController : MonoBehaviour
 {
     [SerializeField] private Button _playButton;
+    [SerializeField] private Button _instructionsButton;
     [SerializeField] private Button _optionsButton;
     [SerializeField] private Button _creditsButton;
     [SerializeField] private Button _exitButton;
     [SerializeField] private Button _backButton;
     [SerializeField] private Image _barImage;
     [SerializeField] private Image _creditsImage;
+    [SerializeField] private Text _instructionText;
     [SerializeField] private GameObject _optionsPanel;
     [SerializeField] private Toggle _soundToggle;
     [SerializeField] private Toggle _musicToggle;
@@ -28,6 +30,7 @@ public class MainMenuController : MonoBehaviour
     {
         SoundController._instance.PlayClick();
         _playButton.gameObject.SetActive(false);
+        _instructionsButton.gameObject.SetActive(false);
         _optionsButton.gameObject.SetActive(false);
         _creditsButton.gameObject.SetActive(false);
         _exitButton.gameObject.SetActive(false);
@@ -40,11 +43,13 @@ public class MainMenuController : MonoBehaviour
     {
         SoundController._instance.PlayClick();
         _playButton.gameObject.SetActive(true);
+        _instructionsButton.gameObject.SetActive(true);
         _optionsButton.gameObject.SetActive(true);
         _creditsButton.gameObject.SetActive(true);
         _exitButton.gameObject.SetActive(true);
         _backButton.gameObject.SetActive(false);
         _creditsImage.gameObject.SetActive(false);
+        _instructionText.gameObject.SetActive(false);
         _barImage.transform.position = new Vector3(-1920.0f, 0.0f, 0.0f);
     }
 
@@ -52,6 +57,7 @@ public class MainMenuController : MonoBehaviour
     {
         SoundController._instance.PlayClick();
         _playButton.gameObject.SetActive(false);
+        _instructionsButton.gameObject.SetActive(false);
         _optionsButton.gameObject.SetActive(false);
         _creditsButton.gameObject.SetActive(false);
         _exitButton.gameObject.SetActive(false);
@@ -65,10 +71,24 @@ public class MainMenuController : MonoBehaviour
     {
         SoundController._instance.PlayClick();
         _playButton.gameObject.SetActive(true);
+        _instructionsButton.gameObject.SetActive(true);
         _optionsButton.gameObject.SetActive(true);
         _creditsButton.gameObject.SetActive(true);
         _exitButton.gameObject.SetActive(true);
         _optionsPanel.SetActive(false);
+        _barImage.transform.position = new Vector3(-1920.0f, 0.0f, 0.0f);
+    }
+
+    public void DisplayInstructions()
+    {
+        SoundController._instance.PlayClick();
+        _playButton.gameObject.SetActive(false);
+        _instructionsButton.gameObject.SetActive(false);
+        _optionsButton.gameObject.SetActive(false);
+        _creditsButton.gameObject.SetActive(false);
+        _exitButton.gameObject.SetActive(false);
+        _backButton.gameObject.SetActive(true);
+        _instructionText.gameObject.SetActive(true);
         _barImage.transform.position = new Vector3(-1920.0f, 0.0f, 0.0f);
     }
 
